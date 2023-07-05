9c6831e29f973fd9d3c8be1ccc351739af6f23b3
Andrew Morrow <beardedmongo@gmail.com>
2023-07-05 16:03:34 -0500

    Adds material ui

---
+++ src/theme.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { Roboto } from "next/font/google";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+import { ReactNode } from "react";
+import { CssBaseline } from "@mui/material";
+
+export const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+});
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
+
+const MyThemeProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default MyThemeProvider;
